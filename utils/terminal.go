e2f3dce93b9a13a8480092fd3bb71ed32a51ccfe
socketo <sock3t0@gmail.com>
2023-11-17 23:23:42 +0900

    remove oscommand clearTerminal

--- utils/terminal.go
+++ utils/terminal.go
@@ -4,23 +4,6 @@ import (
 	"fmt"
 )
 
-/*
-// ClearTerminal terminalの画面クリア
-
-	func ClearTerminal() {
-		switch runtime.GOOS {
-		case "windows":
-			cmd := exec.Command("cmd", "/c", "cls")
-			cmd.Stdout = os.Stdout
-			cmd.Run()
-		default:
-			cmd := exec.Command("clear")
-			cmd.Stdout = os.Stdout
-			cmd.Run()
-		}
-	}
-*/
-
 // ClerTerminal terminalの画面クリア(ANSIエスケープコード版)
 func ClearTerminal() {
 	fmt.Print("\033[H\033[2J") // ANSIエスケープコード
